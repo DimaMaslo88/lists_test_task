14c1dd3e895a0fa61ab436f48c6cea1a7eba7a01
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-11-14 21:10:21 +0300

    "first commit"

---
+++ src/bll/actions/userTreeActions.ts
@@ -0,0 +1,11 @@
+import {UserTreeReducerType} from "bll/reducers/userPostsReducer";
+
+export type SetUserTreeType = ReturnType<typeof setUserTree>
+export const setUserTree =(data:UserTreeReducerType)=>{
+    return {
+        type:'SET-USER-TYPE',
+        payload:{
+            data
+        }
+    }as const
+}
--- src/bll/reducers/userPostsReducer.ts
+++ src/bll/reducers/userPostsReducer.ts
@@ -1,21 +1,26 @@
+import {SetUserTreeType} from "bll/actions/userTreeActions";
 
-export const  userTreeReducerState={
+export const userTreeReducerState = {
     id: 0,
     name: "",
     children: [] as ChildrenTreeType[]
 }
-export type ChildrenTreeType ={
-    id:number,
-    name:string
+export type ChildrenTreeType = {
+    id: number,
+    name: string
 }
 export type UserTreeReducerType = {
-    id:number,
-    name:string,
-    children:ChildrenTreeType[]
+    id: number,
+    name: string,
+    children: ChildrenTreeType[]
 
 
 }
+export type UserTreeActionsType = SetUserTreeType
 
-export const UserTreeReducer =(state:any,action:any)=>{
-
+export const UserTreeReducer = (state: UserTreeReducerType = userTreeReducerState, action: UserTreeActionsType): UserTreeReducerType => {
+    switch (action.type) {
+        default:
+            return state
+    }
 }
